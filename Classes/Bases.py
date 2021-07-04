5bd5e0683750a4984e9431c98fff2e8a7359168c
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-07-04 23:05:22 +0300

    calling class and basses, adding sounds and level pictures....collaborative work with my team

--- Classes/Bases.py
+++ Classes/Bases.py
@@ -1,6 +1,31 @@
-import pygame,sys,random
-from pygame.math import Vector2
-
+random_number=random.randint(0, 9)
+snake_color_list=['blue','black','grey','green','light-blue','orange','red','yellow','purple','brown','brown']
+primary_board_color=[(175, 215, 70),(200, 188, 177),(67, 31, 29),(250, 183, 61),(45, 6, 37),(244, 215, 94),(195, 49, 36),(211, 220, 200),(192, 4, 123),(120, 63, 4)]
+secondary_board_color=[(167, 209, 61),(228, 218, 207),(144, 88, 75),(253, 220, 165),(33, 65, 12),(153, 41, 21),(110, 5, 22),(175, 202, 84),(255, 6, 89),(120, 63, 4)]
+pn = 0
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init()
 cell_size = 40
 cell_number = 20
-screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
+score = 0
+level_name = 'level 1'
+snake_color = snake_color_list[0]
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+pygame.display.set_caption('United - Snake Game')
+clock = pygame.time.Clock()
+fruit_score = pygame.image.load('Graphics/Fruits/fruits.png').convert_alpha()
+game_font = pygame.font.Font('Font/Astral Groove.ttf', 20)
+rules_font = pygame.font.Font('Font/Astral Groove.ttf', 10)
+point_font = pygame.font.Font('Font/Space Rave.ttf', 30)
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+SCREEN_UPDATE2 = pygame.USEREVENT+1
+pygame.time.set_timer(SCREEN_UPDATE2, 4000)
+main_game = MAIN()
+stop_reassgin_background_music=False
+play_intro=True
+playlist = list()
+playlist.append('Sound/Play_ground3.mp3')
+playlist.append('Sound/Play_ground.mp3')
+playlist.append('Sound/Play_ground2.mp3')
--- Classes/Calling.py
+++ Classes/Calling.py
@@ -2,38 +2,114 @@
 from Classes.Bases import *
 from Classes.Main import *
 
-pygame.mixer.pre_init(44100,-16,2,512)
-pygame.init()
-clock = pygame.time.Clock()
-SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE,150)
+def sound_background(flag,page):
+    global  stop_reassgin_background_music
+    if flag != stop_reassgin_background_music and page=='main_minue':
+        pygame.mixer.music.load('Sound/StartWindow.wav')
+        pygame.mixer.music.play(-1, 0)
+        pygame.mixer.music.set_volume(0.2)
+        stop_reassgin_background_music=flag
+    elif flag != stop_reassgin_background_music and page=='play_ground':
+        stop_reassgin_background_music = flag
+        pygame.mixer.music.load(playlist[0])
+        pygame.mixer.music.queue(playlist[1])
+        pygame.mixer.music.set_endevent(pygame.USEREVENT)
+        pygame.mixer.music.play()
+    elif flag != stop_reassgin_background_music and page == 'win_ground':
+        stop_reassgin_background_music = flag
+        pygame.mixer.music.load('Sound/Win_ground.mp3')
+        pygame.mixer.music.play()
+    elif flag != stop_reassgin_background_music and page == 'game_over':
+        stop_reassgin_background_music = flag
+        pygame.mixer.music.load('Sound/game_over.mp3')
+        pygame.mixer.music.play()
 
-main_game = MAIN()
+
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+        if event.type == pygame.USEREVENT:  # A track has ended
+            if len(playlist) > 0 and main_game.play:
+                temp=playlist.pop(0)
+                pygame.mixer.music.queue(temp)
+                playlist.append(temp)
+        if event.type == SCREEN_UPDATE2:
+            main_game.fruit.draw_fruit_po()
+            main_game.fruit.randomize_po()
+            main_game.fruit.randomize_po2()
+            main_game.fruit.randomize_po3()
+            main_game.fruit.randomize_po4()
+            main_game.fruit.randomize_po5()
+            main_game.fruit.randomize_po6()
+            main_game.fruit.randomize_po7()
+            main_game.fruit.randomize_po8()
+            main_game.fruit.randomize_po9()
+            main_game.fruit.randomize_po10()
+        if main_game.play==True:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    if main_game.snake.direction.y != 1:
+                        main_game.snake.direction = Vector2(0, -1)
+                if event.key == pygame.K_RIGHT:
+                    if main_game.snake.direction.x != -1:
+                        main_game.snake.direction = Vector2(1, 0)
+                if event.key == pygame.K_DOWN:
+                    if main_game.snake.direction.y != -1:
+                        main_game.snake.direction = Vector2(0, 1)
+                if event.key == pygame.K_LEFT:
+                    if main_game.snake.direction.x != 1:
+                        main_game.snake.direction = Vector2(-1, 0)
+        else:
+            mousePosition = pygame.mouse.get_pos()
+            if main_game.main_minue_dialog == 1 :
+                if 313 <= mousePosition[0] <= 509 and 563 <= mousePosition[1] <= 654:
+                    pygame.mouse.set_cursor(*pygame.cursors.tri_left)
+                elif 371<=mousePosition[0]<=445 and 462<=mousePosition[1]<=509 :
+                    pygame.mouse.set_cursor(*pygame.cursors.tri_left)
+                else:
+                    pygame.mouse.set_cursor(*pygame.cursors.diamond)
+            elif main_game.main_minue_dialog == 4 :
+                if 71 <= mousePosition[0] <= 130 and 682 <= mousePosition[1] <= 731:
+                    pygame.mouse.set_cursor(*pygame.cursors.tri_left)
+                else:
+                    pygame.mouse.set_cursor(*pygame.cursors.diamond)
+            elif main_game.main_minue_dialog == 3:
+                if 71 <= mousePosition[0] <= 130 and 682 <= mousePosition[1] <= 731:
+                    pygame.mouse.set_cursor(*pygame.cursors.tri_left)
+                elif 313 <= mousePosition[0] <= 509 and 563 <= mousePosition[1] <= 654:
+                    pygame.mouse.set_cursor(*pygame.cursors.tri_left)
+                else:
+                    pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
-	for event in pygame.event.get():
 
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == SCREEN_UPDATE:
-			main_game.update()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
-				if main_game.snake.direction.y != 1:
-					main_game.snake.direction = Vector2(0,-1)
-			if event.key == pygame.K_RIGHT:
-				if main_game.snake.direction.x != -1:
-					main_game.snake.direction = Vector2(1,0)
-			if event.key == pygame.K_DOWN:
-				if main_game.snake.direction.y != -1:
-					main_game.snake.direction = Vector2(0,1)
-			if event.key == pygame.K_LEFT:
-				if main_game.snake.direction.x != 1:
-					main_game.snake.direction = Vector2(-1,0)
+            if event.type == MOUSEBUTTONDOWN:
 
-	screen.fill((175,215,70))
-	main_game.draw_elements()
-	pygame.display.update()
-	clock.tick(60)
+                if event.button == 1 and main_game.main_minue_dialog == 1:
+                    if 313<=mousePosition[0]<=509 and 563<=mousePosition[1]<=654:
+                        main_game.main_minue_dialog = 0
+                        main_game.play=True
+                    elif 371<=mousePosition[0]<=445 and 462<=mousePosition[1]<=509 and main_game.main_minue_dialog == 1:
+                        main_game.main_minue_dialog = 4
 
+                elif event.button == 1 and main_game.main_minue_dialog == 4:
+                    if 71 <= mousePosition[0] <= 130 and 682 <= mousePosition[1] <= 731:
+                        main_game.main_minue_dialog = 1
+                elif event.button == 1 and main_game.main_minue_dialog == 3:
+
+                    if 71 <= mousePosition[0] <= 130 and 682 <= mousePosition[1] <= 731:
+                        main_game.main_minue_dialog = 1
+                    elif 313<=mousePosition[0]<=509 and 563<=mousePosition[1]<=654:
+                        main_game.main_minue_dialog = 0
+                        main_game.play=True
+
+
+
+
+    screen.fill((240,240,240))
+    main_game.draw_elements()
+    pygame.display.update()
+    clock.tick(60)
